49cfad404e705020f6469d5a05d82a2315ec7529
Miro Manestar <miroimanestar@msn.com>
2020-07-08 22:51:18 -0400

    Changed clock to UTC

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,16 @@ $(document).ready(function () {
 
 function startEDClock() {
     var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
+    var h = today.getUTCHours();
+    var m = today.getUTCMinutes();
+    var s = today.getUTCSeconds();
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('edtime').innerHTML =
     h + ":" + m + ":" + s;
     var t = setTimeout(startEDClock, 500);
-  }
+}
+  
   function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
