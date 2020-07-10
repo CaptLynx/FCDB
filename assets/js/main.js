2c36b49238d9c4e904b35f9e8abb048faabac334
Miro Manestar <miroimanestar@msn.com>
2020-07-10 00:25:44 -0400

    Added responsive navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     startEDClock();
 });
 
+window.addEventListener('scroll', function() {
+    $('header').toggleClass('sticky', window.scrollY > 0);
+});
+
 function startEDClock() {
     var today = new Date();
     var h = today.getUTCHours();
@@ -9,14 +13,12 @@ function startEDClock() {
     var s = today.getUTCSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('edtime').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startEDClock, 500);
+    $('#edtime').html(`${ h }:${ m }:${ s }`)
+    setTimeout(startEDClock, 500);
 }
-  
-  function checkTime(i) {
+
+function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
-  }
+}
 
-  
