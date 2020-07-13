de17d4fcf3da4d4f42b7da1c37f6ef1fc2d3125b
Miro Manestar <miroimanestar@msn.com>
2020-07-13 15:53:03 -0400

    Added SPA functionality

--- assets/js/partialhtml.js
+++ assets/js/partialhtml.js
@@ -8,9 +8,9 @@ function partialhtml(selection) {
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
                     if (this.status == 200) {
-                        $('section.content').innerHTML = this.responseText;
+                        $('#page-content').html(this.responseText);
                     }
-                    if (this.status == 404) { $('section.content').innerHTML = `Error loading partial ${ file }`; }
+                    if (this.status == 404) { $('#page-content').innerHTML = `Error loading partial ${ file }`; }
 
                 }
             }
